d0aaa5df2cea8f017902e284a56d9b70d9851c28
Derrick Dryer <derrickdryer@proton.me>
2025-02-05 23:02:38 -0500

    Update pi_rsync.sh
    Changed top description of script

--- pi_rsync.sh
+++ pi_rsync.sh
@@ -1,23 +1,28 @@
-#!/bin/bash 
-# PiHole Sync
+#!/bin/bash
+# PiHole Sync Keyfile
 # Sync Two PiHoles from Master
-#
+# Utilizes keyfile for secure authenication instead of password
+# derrickdryer - https://www.derrickdryer.xyz
+# 
+# vvvvvvvvvvvvvvvvvvvvvvvvv
 # JejjE - https://jejje.net
 # https://jejje.net/2021-01-30-sync-two-pihole-dns-servers-for-failover
 # Credit for the idea: https://pastebin.com/KFzg7Uhi
-# 
+#
 # This script makes use of two packages RSYNC, SSHPASS
 # you will need to install them first.
 
 #### Setup Vars ####
 # What files to RSYNC
-SYNCFILES=(gravity.db custom.list dhcp.leases local.list) 
+SYNCFILES=(gravity.db custom.list dhcp.leases local.list)
 # The Secondary PiHole Host
-HOST=192.168.0.114
+HOST=192.168.0.X
 # Username on Secondary HOST
-USER=pi
+USER=USER
 # Password on Secondary HOST, dont forget to change password
-PSW=rasberry
+PSW="PASSWORD"
+# RSA Keyfile
+IDENTITY_FILE="/path/to/key"
 # Update interval for CRONJOB in minutes
 CRONTIME=15
 
@@ -25,64 +30,63 @@ CRONTIME=15
 # Print help
 function print_help()
 {
-	echo -e "\e[32mSyntax: $0 [-i] [-s]"
-	echo -e "\e[32m\t[-i]\e[39m install: Set up Cronjob, only run once"
-	echo -e "\e[32m\t[-s]\e[39m sync: Syncing the files"
-	exit 1
+        echo -e "\e[32mSyntax: $0 [-i] [-s]"
+        echo -e "\e[32m\t[-i]\e[39m install: Set up Cronjob, only run once"
+        echo -e "\e[32m\t[-s]\e[39m sync: Syncing the files"
+        exit 1
 }
 
 # Set up Cronjob
 function process_install()
 {
-	echo -e "Adding \e[32mCRONJOB\e[39m to crontab..."
-	# Make temporary cronfile
-	crontab -l > mycron
-	# Echo into file
-	echo -e "*/$CRONTIME * * * * /bin/bash /etc/pihole/pi_rsync.sh -s" >> mycron # Cron runs every 15 minutes
-	# Install from the temporary cron file
-	crontab mycron
-	# Clean it up
-	rm mycron
+        echo -e "Adding \e[32mCRONJOB\e[39m to crontab..."
+        # Make temporary cronfile
+        crontab -l > mycron
+        # Echo into file
+        echo -e "*/$CRONTIME * * * * /bin/bash /etc/pihole/pi_rsync.sh -s" >> mycron # Cron runs every 15 minutes
+        # Install from the temporary cron file
+        crontab mycron
+        # Clean it up
+        rm mycron
 }
 
 # Syncing the files
 function process_sync()
 {
 
-	# Stop the pihole service 
-	echo -e "Running command \e[96mservice pihole-FTL stop\e[39m on \e[32m{$HOST}\e[39m"
-	sshpass -p "$PSW" ssh  $USER@$HOST "echo $PSW | sudo -S service pihole-FTL stop"
+        # Stop the pihole service
+        echo -e "Running command \e[96mservice pihole-FTL stop\e[39m on \e[32m{$HOST}\e[39m"
+        sshpass -p "$PSW" ssh -i $IDENTITY_FILE  $USER@$HOST "echo $PSW | sudo -S service pihole-FTL stop"
 
-	# Loop trough files and RSYNC them to the Secondary Host
-	for FILE in "${SYNCFILES[@]}"
-	do
-		echo -e "Syncing \e[32m{$FILE}\e[39m over to \e[32m{$HOST}\e[39m..."
-		# Needs additional '-rsync-path="sudo rsync"' param if $USER is not root
-		RSYNC=$(sshpass -p "$PSW" rsync /etc/pihole/$FILE $USER@$HOST:/etc/pihole/)		
-	done
+        # Loop trough files and RSYNC them to the Secondary Host
+        for FILE in "${SYNCFILES[@]}"
+        do
+                echo -e "Syncing \e[32m{$FILE}\e[39m over to \e[32m{$HOST}\e[39m..."
+                # Needs additional '-rsync-path="sudo rsync"' param if $USER is not root
+                RSYNC=$(sshpass -p "$PSW" rsync -e "ssh -i $IDENTITY_FILE" /etc/pihole/$FILE $USER@$HOST:/etc/pihole/)
+        done
 
-	# Do updates, reload and restart dns
-	echo -e "Running command \e[96mpkill pihole-FTL\e[39m on \e[32m{$HOST}\e[39m"
-	sshpass -p "$PSW" ssh  $USER@$HOST "echo $PSW | sudo -S pkill pihole-FTL"
-	echo -e "Running command \e[96mservice pihole-FTL start\e[39m on \e[32m{$HOST}\e[39m"
-	sshpass -p "$PSW" ssh  $USER@$HOST "echo $PSW | sudo -S service pihole-FTL start"
-	echo -e "Running command \e[96mpihole restartdns reload-lists\e[39m on \e[32m{$HOST}\e[39m"
-	sshpass -p "$PSW" ssh  $USER@$HOST "echo $PSW | sudo -S pihole restartdns reload-lists"
+        # Do updates, reload and restart dns
+        echo -e "Running command \e[96mpkill pihole-FTL\e[39m on \e[32m{$HOST}\e[39m"
+        sshpass -p "$PSW" ssh -i $IDENTITY_FILE $USER@$HOST "echo $PSW | sudo -S pkill pihole-FTL"
+        echo -e "Running command \e[96mservice pihole-FTL start\e[39m on \e[32m{$HOST}\e[39m"
+        sshpass -p "$PSW" ssh -i $IDENTITY_FILE $USER@$HOST "echo $PSW | sudo -S service pihole-FTL start"
+        echo -e "Running command \e[96mpihole restartdns reload-lists\e[39m on \e[32m{$HOST}\e[39m"
+        sshpass -p "$PSW" ssh -i $IDENTITY_FILE $USER@$HOST "echo $PSW | sudo -S pihole restartdns reload-lists"
 }
 
 # Print help if no argument is supplied
 if [[ $# -eq 0 ]] ; then
-	print_help
-	exit 0
+        print_help
+        exit 0
 fi
 
 # Catch arguments
 while getopts ":is" o
 do
-	case "$o" in
-	i) process_install ;;
-	s) process_sync ;;  
-	*)
-	esac
+        case "$o" in
+        i) process_install ;;
+        s) process_sync ;;
+        *)
+        esac
 done;
-
